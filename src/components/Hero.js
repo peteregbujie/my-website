a93eeed8f6eafdc452f1e7b9256b8d8233a9882f
Peter Egbujie <ogbillion@gmail.com>
2022-02-28 16:11:34 -0800

    Improved Hero design

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -3,27 +3,58 @@ import photo from '../../public/potrait.jpeg';
 
 function Hero() {
   return (
-   <section className='text-gray-600 body-font'>
-    <div className='container mx-auto flex px-5 py-24 md:flex-row flex-col justify-center items-center lg:mx-12'>
-     <div className='lg:flex-grow lg:w-1/2 lg:max-w-2xl md:w-full lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center mx-8'>
-      <h1 className='title-font sm:text-4xl text-4xl mb-4 font-bold text-gray-900 dark:text-white'>
-       Making the web a better place, one line of code at a time.
+   <section className='text-gray-600'>
+    <div className='container mx-auto flex flex-col justify-center items-center px-5 py-24 lg:flex-row  lg:mx-12'>
+     <div className='flex-1 lg:w-1/2  md:w-full lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center mx-4'>
+      <h1 className='text-4xl lg:text-6xl mb-4 font-bold text-gray-900 dark:text-white'>
+       Hi, i&apos;m Peter Egbujie
        <br />
       </h1>
-      <span className='text-2xl mb-4 font-medium dark:text-gray-200'>
-       Peter Egbujie, JavaScript Developer
+      <span className='text-4xl mb-4 font-medium dark:text-white'>
+       Frontend Developer
       </span>
-      <p className='mb-8 leading-loose dark:text-gray-300'>
-       Welcome to my website where I share about learning JavaScript, building
-       products, becoming a better developer and growing a career in tech.
+      <p className='mb-8 dark:text-gray-100 text-2xl text-justify leading-10 max-w-prose'>
+       Welcome to my website where I share what i&apos;m learning about
+       JavaScript, building products, becoming a better developer and growing a
+       career in tech.
       </p>
-      <div className='flex justify-center'>
-       <a href='https://www.linkedin.com/in/peter-egbujie/'>
-        <button className='btn-1'>View on LinkedIn</button>
-       </a>
-       <a href='https://github.com/peteregbujie' className='ml-8'>
-        <button className='btn-2'>View Github</button>
-       </a>
+      <div className='flex justify-center dark:text-white'>
+       <abbr
+        title='GitHub'
+        className='pr-5 text-4xl font-bold hover:text-gray-900 no-underline'
+       >
+        <a
+         href='https://github.com/peteregbujie'
+         target='_blank'
+         rel='noreferrer'
+        >
+         GH
+        </a>
+       </abbr>
+       <abbr
+        title='Twitter'
+        className='pr-5 text-4xl font-bold hover:text-gray-900 no-underline'
+       >
+        <a
+         href='https://twitter.com/peteregbujie'
+         target='_blank'
+         rel='noreferrer'
+        >
+         TW
+        </a>
+       </abbr>
+       <abbr
+        title='LinkedIn'
+        className='pr-5 text-4xl font-bold hover:text-gray-900 no-underline'
+       >
+        <a
+         href='https://www.linkedin.com/in/peter-egbujie/'
+         target='_blank'
+         rel='noreferrer'
+        >
+         LI
+        </a>
+       </abbr>
       </div>
      </div>
      <div className='hidden lg:block max-w-[300px] mr-12'>

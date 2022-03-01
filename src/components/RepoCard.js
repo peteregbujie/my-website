b01cdaf95dc0a8c5aa85ee6a7d5c1b2dc6777fa5
Peter Egbujie <ogbillion@gmail.com>
2022-02-28 20:25:52 -0800

    optimized card for mobile

--- src/components/RepoCard.js
+++ src/components/RepoCard.js
@@ -2,8 +2,8 @@ import React from 'react';
 
 function RepoCard({title,description,github,live}) {
     return (
-     <div className='container mb-4 mx-4 flex flex-col justify-center items-center'>
-      <div className='p-4  lg:w-1/2 lg:ml-20 md:w-full  rounded-md border-2 border-gray-800 dark:border-gray-200 text-left  hover:shadow-[12px_12px_2px_1px_rgba(0,0,0,0.5)] dark:hover:shadow-[12px_12px_2px_1px_rgba(255,255,255)]'>
+     <div className='container flex flex-col justify-center items-center'>
+      <div className='p-4 mx-4 mb-4 lg:w-1/2 lg:ml-20 md:w-full  rounded-md border-2 border-gray-800 dark:border-gray-200 text-left  hover:shadow-[12px_12px_2px_1px_rgba(0,0,0,0.5)] dark:hover:shadow-[12px_12px_2px_1px_rgba(255,255,255)]'>
        <h2 className='text-gray-800 text-2xl title-font font-medium mb-2 dark:text-gray-200'>
         {title}
        </h2>
@@ -14,9 +14,7 @@ function RepoCard({title,description,github,live}) {
         </a>
         {live && (
          <a href={live} className='ml-8'>
-          <button className='btn-2'>
-           Live Demo
-          </button>
+          <button className='btn-2'>Live Demo</button>
          </a>
         )}
        </div>

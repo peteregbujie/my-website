a6fe6f9dccbc41d5ee90fefc449992ce1dfe2f25
Peter Egbujie <ogbillion@gmail.com>
2022-06-11 09:20:29 -0700

    Moved card content to JSON file

--- src/data/data.js
+++ src/data/data.js
@@ -1,14 +1,22 @@
-export const cardOne = {
- title: 'Landing Page for a Food Bank',
- description: 'Made with ReactJs. Fully responsive design.',
- github: 'https://github.com/peteregbujie/foodbank-landing-page',
-};
-
-export const cardTwo = {
- title: 'Recipe Finder',
- description:
-  'Recipe app built with NextJS. Fetch meals from the themealdb API using axios + useSWR for data fetching. Checkout the live demo with the link below.', 
- github: 'https://github.com/peteregbujie/nextjs-recipe-app',
- live: 'http://nextjs-recipe-app-phi.vercel.app/',
-};
-
+export const cards = [
+ {
+  title: "NextJS Ecommerce App",
+  description:
+   "Full Stack ECommerce store built with NextJS, MongoDB, React Query, Stripe checkout  and Tailwind CSS. Products are prefetched at build time with react query so as to reduce the number of api requests.",
+  github: "https://github.com/peteregbujie/nextjs_react_query_ecommerce",
+  live: "http://nextjs-react-query-ecommerce.vercel.app/",
+ },
+ {
+  title: "Landing Page for a Food Bank",
+  description: "Made with ReactJs. Fully responsive design.",
+  github: "https://github.com/peteregbujie/foodbank-landing-page",
+  live: "",
+ },
+ {
+  title: "Recipe Finder",
+  description:
+   "Recipe app built with NextJS. Fetch meals from the themealDB API using axios + useSWR for data fetching. Checkout the live demo with the link below.",
+  github: "https://github.com/peteregbujie/nextjs-recipe-app",
+  live: "http://nextjs-recipe-app-phi.vercel.app/",
+ },
+];
